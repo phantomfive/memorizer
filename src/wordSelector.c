f0a5e471e0f696b6e39fe857712279ecc4bb67e3
Andrew Thompson <andrewthompson10@gmail.com>
2012-11-12 00:22:59 -0800

    Finally wrote function chooseNextWordGroup(). Ugh, that was a hard one.

--- src/wordSelector.c
+++ src/wordSelector.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "database.h"
 
 #include "wordSelector.h"
 
-static WordGroupType chooseNextWordGroup();
+//private methods
+static WordGroupType chooseNextWordGroup(int quantity, int index);
 static BOOL getWordForGroupA(WordForReview **word, int index);
 static BOOL getWordForGroupB(WordForReview **word, int index);
 static BOOL getWordForGroupC(WordForReview **word, int index);
@@ -18,15 +20,16 @@ BOOL selectWordsForReview(WordForReview **words, int quantity) {
 	BOOL rv = FAIL;
 
 	for(i=0;i<quantity;i++) {
-		WordGroupType type = chooseNextWordGroup();
+		WordGroupType type = chooseNextWordGroup(quantity, i);
 	
-		if(type==WordGroupD)             rv=getWordForGroupD(words, i);
+		if(type==WordGroupE)             rv=getWordForGroupE(words, i);
+		if(type==WordGroupD || rv==FAIL) rv=getWordForGroupD(words, i);
 		if(type==WordGroupC || rv==FAIL) rv=getWordForGroupC(words, i);
 		if(type==WordGroupB || rv==FAIL) rv=getWordForGroupB(words, i);
 		if(type==WordGroupA || rv==FAIL) rv=getWordForGroupA(words, i);
 
 		if(rv==FAIL) {
-			printf("Not enough words\n");
+			printf("Failed to get word\n");
 			return FAIL;
 		}
 	}
@@ -42,24 +45,61 @@ void markWordAsReviewed(WordForReview *word, BOOL result) {
 //---------------------------------------------------------------------------------
 // Private methods
 //---------------------------------------------------------------------------------
-static WordGroupType chooseNextWordGroup() {
-	return WordGroupD;
+static int wordsReturned = 0;
+#define CYCLE_POINT 8
+#define NEW_WORDS_TO_REVIEW_AT_A_TIME 10
+static WordGroupType chooseNextWordGroup(int quantity, int index) {
+	int groupBcount = databaseGetCountForWordGroup(WordGroupB);
+	int groupCcount = databaseGetCountForWordGroup(WordGroupC);
+
+	//return at least one word from groupE and one from groupD every cycle
+	int cycle = wordsReturned % CYCLE_POINT;
+	wordsReturned++;
+	if(cycle == 0) return WordGroupE;
+	if(cycle == 1) return WordGroupD;
+	
+	//If we have learned all the words in our review set, then
+	//get some more from group A and start learning them!
+	if(groupBcount+groupCcount < NEW_WORDS_TO_REVIEW_AT_A_TIME) 
+		return WordGroupA;
+
+	//choose a word randomly from group B or C
+	if(rand()%2==0)
+		return WordGroupB;
+	else
+		return WordGroupC;
 }
+
 
 static BOOL getWordForGroupA(WordForReview **word, int index) {
 	return databaseFillWordFromGroup(word, index, WordGroupA);
 }
 
 static BOOL getWordForGroupB(WordForReview **word, int index) {
-	return databaseFillWordFromGroupOrderByLeastRecent(word, index, WordGroupB);
+	if(rand()%2==0)
+		return databaseFillWordFromGroupOrderByLeastRecent(word, index, WordGroupB);
+	else
+		return databaseFillWordFromGroupOrderByLeastSkilled(word, index, WordGroupB);
 }
 
 static BOOL getWordForGroupC(WordForReview **word, int index) {
-	return databaseFillWordFromGroupOrderByLeastRecent(word, index, WordGroupC);
+	if(rand()%2==0)
+		return databaseFillWordFromGroupOrderByLeastRecent(word, index, WordGroupC);
+	else
+		return databaseFillWordFromGroupOrderByLeastSkilled(word, index, WordGroupD);
 }
+
 
 static BOOL getWordForGroupD(WordForReview **word, int index) {
-	return databaseFillWordFromGroupOrderByLeastSkilled(word, index, WordGroupD);
+	if(rand()%2==0)
+		return databaseFillWordFromGroupOrderByLeastSkilled(word, index, WordGroupD);
+	else
+		return databaseFillWordFromGroupOrderByLeastRecent(word, index, WordGroupD);
 }
 
-
+static BOOL getWordForGroupE(WordForReview **word, int index) {
+	if(rand()%2==0)
+		return databaseFillWordFromGroupOrderByLeastSkilled(word, index, WordGroupE);
+	else
+		return databaseFillWordFromGroupOrderByLeastRecent(word, index, WordGroupE);
+}
