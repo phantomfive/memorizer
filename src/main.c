c27247cceb0ce8eb667cb97ea6fbbd3d625e157e
Andrew Thompson <andrewthompson10@gmail.com>
2012-12-20 10:06:29 -0800

    Changed that color from yellow to green because the yellow was too agitating.

--- src/main.c
+++ src/main.c
@@ -61,7 +61,7 @@ static BOOL readTypedInput(char *buf, int bufLen) {
 static void reviewBasicWord(WordForReview *word) {
 	char input[1000];
 	printf("----\n");
-	printf("The %s word is " YELLOW_ON_BLACK "%s" NORMAL_COLOR
+	printf("The %s word is " GREEN_ON_BLACK "%s" NORMAL_COLOR
 	       ".\n   Can you guess the English word? --> ",
 	       language, word->foreignWord);
 
@@ -87,7 +87,7 @@ static void reviewBasicWord(WordForReview *word) {
 static void reviewAdvancedWord(WordForReview *word) {
 	char input[1000];
 	printf("----\n");
-	printf("The English word is " YELLOW_ON_BLACK "%s" NORMAL_COLOR
+	printf("The English word is " GREEN_ON_BLACK "%s" NORMAL_COLOR
 	       ".\n Can you guess the %s word? --> ",
 	        word->localWord, language);
 
